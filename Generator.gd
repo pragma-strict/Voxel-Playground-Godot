1ff321f8fd52a1586def88835dadacbe86be982e
Ian DeJong <ianrdejong@gmail.com>
2021-02-20 01:25:02 -0800

    Fixed voxel scaling

--- Generator.gd
+++ Generator.gd
@@ -14,6 +14,7 @@ var stride_z
 
 var cells = [] # [x][y][z]
 var cell_nodes = [] # Node refs
+var b_generated = false
 
 func _ready():
 	RNG.randomize()
@@ -22,9 +23,12 @@ func _ready():
 	stride_z = 1
 
 func _input(event):
-	if(Input.is_key_pressed(KEY_SPACE)):
+	if(Input.is_key_pressed(KEY_SPACE) and !b_generated):
 		generate_cells()
 		generate_cell_nodes()
+		b_generated = true
+	if(Input.is_key_pressed(KEY_A)):
+		print_tree_pretty()
 
 func generate_cells():
 	for i in range(chunk_width * chunk_width * chunk_height):
@@ -36,11 +40,12 @@ func generate_cell_nodes():
 		var node = MeshInstance.new()
 		node.mesh = CubeMesh.new()
 		var position = cell_index_to_coordinate(i) * cell_size
-		#TODO Scale down mesh or node
 		node.translate(position)
+		node.scale = Vector3(cell_size * 0.1, cell_size * 0.1, cell_size * 0.1)
 		add_child(node)
 		print("Created cube mesh at: ", position)
 
+#TODO Make this function actually work. Or, generally, figure out how the cubes are actually being displayed.
 func cell_index_to_coordinate(index:int):
 	var coordinate = Vector3()
 	coordinate.x = floor(index / stride_x)
